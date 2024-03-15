645b1c1749346b841b01db5bd7115e6f9de66f47
Olga Novak <olga_novak@epam.com>
2024-03-15 07:36:04 +0100

    update api_paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,12 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product:
+    "https://o-novak-shop-react-redux-cloudfront.execute-api.eu-west-1.amazonaws.com/dev",
+  order:
+    "https://o-novak-shop-react-redux-cloudfront.execute-api.eu-west-1.amazonaws.com/dev",
+  import:
+    "https://o-novak-shop-react-redux-cloudfront.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://o-novak-shop-react-redux-cloudfront.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://o-novak-shop-react-redux-cloudfront.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
